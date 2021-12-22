5b31b7e45f089b8f1a21b31d091f3d81fa4467d2
Vinay Avasthi <avasthi.vinay88@gmail.com>
2021-12-22 14:31:05 +0530

    add article method

--- react-blog-backend/src/index.js
+++ react-blog-backend/src/index.js
@@ -11,81 +11,86 @@ const port = process.env.PORT || 3000
 const host = process.env.HOST || "localhost"
 
 
-const withDB = async ( operations, res) => {
+const withDB = async (operations, res) => {
   const client = MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
-  client.then((x) => {
-    x.connect();
+  client.then(async (x) => {
+    console.log("in try");
+    //x.connect();
     const db = x.db('blog');
-    operations(db);
+    await operations(db);
   }).catch((error) => {
+    console.log("in catch");
+    console.log(error);
     res.status(500).json({ message: 'Error connecting to db', error });
   }).finally(() => {
-     client.then((x) => x.close());
+    console.log("in finally");
+    client.then((x) => x.close());
   });
 }
+
+// const withDB = async (operations, res) => {
+//   try {
+//     const client = await MongoClient.connect(process.env.MONGODB_URL, { useNewUrlParser: true });
+//     const db = client.db('blog');
+
+//     await operations(db);
+
+//     client.close();
+//   } catch (error) {
+//     res.status(500).json({ message: 'Error connecting to db', error });
+//   }
+// }
 
 const app = express()
 //app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
+  res.send('Hello World!');
+})
 
+app.get('/api/articles/imports', (req,res)=> {
   withDB(async (dbo) => {
     const _articles = dbo.collection("articles"); // do this INSIDE async function
     const options = { ordered: true };
     const result = await _articles.insertMany(articles, options);
     console.log(`${result.insertedCount} documents were inserted`);
   }, res);
-
-  // withDB(async (dbo) => {
-  //   const collection = await DB.collection("articles"); // do this INSIDE async function
-
-  //   await collection.find({}).limit(5)
-  //     .toArray(function (err, result) {
-  //       if (err) throw err;
-  //       //res.send(JSON.stringify(result));
-  //       res.status(200).json(result);
-  //       dbo.close();
-  //     });
-  // }, res);
-
-
-  // const client = new MongoClient(process.env.MONGODB_URL, { useNewUrlParser: true });
-  // client.connect((err) => {
-  //   if (err) throw err;
-
-  //   const db = client.db('local');
-  //   const collection = db.collection('startup_log').find({}).limit(5)
-  //     .toArray(function (err, res) {
-  //       if (err) throw err;
-
-  //       console.log(JSON.stringify(res));
-
-  //       db.close();
-  //     });;
-
-  //   //console.log(JSON.stringify(collection));
-  //   client.close();
-  // });
-
-  res.send('Hello World!');
 })
 
 app.get('/api/articles', (req, res) => {
-  res.send('Hello World!');
-})
-app.get('/api/article/:id', (req, res) => {
-  res.send('Hello World!');
-})
-app.get('/api/article/:id/comments', (req, res) => {
-  res.send('Hello World!');
+  withDB(async (db) => {
+    const _articles = await db.collection("articles");
+    const articles = _articles.find({});
+
+    if ((await articles.count()) === 0) {
+      console.log("No documents found!");
+    }
+
+    const articlesl = [];
+
+    await articles.forEach((article, i) => {
+      articlesl.push(article)
+    });
+
+    res.status(200).json(articlesl);
+  }, res);
 })
 
-// app.get('*', (req, res) => {
-//   //res.sendFile(path.join(__dirname + '/index.html'));
-//   res.send('{}');
-// });
+app.get('/api/article/:name', (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+    console.log(req.params.name);
+    const articleInfo = await db.collection('articles').findOne({ name: articleName })
+    res.status(200).json(articleInfo);
+  }, res);
+})
+
+app.get('*', (req, res) => {
+  //res.sendFile(path.join(__dirname + '/index.html'));
+  res.send('{}');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://${host}:${port}`);
+  console.log(`app listening at http://${host}:${port}`);
 })
